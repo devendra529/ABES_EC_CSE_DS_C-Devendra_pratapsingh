047c61324f2964799cc0fd5be3d98c06bf7c5b6d
Devendra Pratap singh <167605566+devendra529@users.noreply.github.com>
2026-02-19 10:27:01 +0530

    Add files via upload

---
+++ ABES-DS-6-SEM-FSD-main/day_04/read.js
@@ -0,0 +1,7 @@
+import{readFile} from "./readANDWrite.js";
+const FILE = "/students.json";
+const readData = async() => {
+    const studentData = await readFile(FILE);
+    console.log("Student Data:", studentData);
+}
+readData();
